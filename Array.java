b4e1384bfa38dd09f480257ecf83fc4ae4de4f2b
CanRahulCode <avulapatirahul@gmail.com>
2023-09-15 12:32:51 +0530

    Creating a array

---
+++ Array.java
@@ -0,0 +1,29 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		int[] num = {1, 2, 3, 4, 5};
+		System.out.print("Done");
+		
+	}
+}
+/*
+ Question
+ 
+Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+
+To declare an array,
+
+Define the variable type followed by square brackets,
+Specify the name of the array and
+Specify the number of elements it should store.
+For eg
+
+ String[] chocolate = {"Ferrero Rocher", "Alpenliebe", "Cadbury"};
+Task
+Write a program which does the following
+
+Create an Array of the first 5 positive integers
+Once the array is define - output "Done" to the console.
+*/
