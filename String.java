81a4cad5e2acac6a92a11dba85f8bccf05cad29d
CanRahulCode <avulapatirahul@gmail.com>
2023-09-15 12:27:21 +0530

    Creating a String

---
+++ String.java
@@ -0,0 +1,26 @@
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String fruit = "Apple";
+		System.out.print(fruit);
+	}
+}
+
+/*
+ Question
+ Creating a String
+ Strings are used for storing text.
+A String variable contains a collection of characters surrounded by double quotes.
+Before declaring a variable as string the String datatype must be used
+
+ for eg - String name = "Chef";
+Task
+Write a program which does the following
+
+Create a string named 
+fruit and assign the value "Apple" to it.
+Output-
+fruit to the console.
+ */
